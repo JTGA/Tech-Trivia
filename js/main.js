ce1b87a5b79c83287487887083421cf4acf00481
J T <njthood@yahoo.com>
2015-10-15 18:00:55 -0700

    thurs

--- js/main.js
+++ js/main.js
@@ -1,46 +1,46 @@
 console.log("JAVASCRIPT Loaded!");
 
 var questions = [{
-    question: "Who is JavaScript officially managed by?",
-    choices: ["Mozilla", "Microsoft", "Apple", "Sun Microsystems"],
-    correctAnswer: 0
-  }, {
-    question: "What is Error 404?",
-    choices: ["Request Timeout", "Site not found in HTTP", "Bad File Request/Syntax Error", "Site Access Forbidden/Password Protected"],
-    correctAnswer: 1
-  }, {
-    question: "Which if the following is correct?",
-    choices: ["jQuery is a JSON library", "jQuery is a JavaScript library"],
-    correctAnswer: 1
-  }, {
-    question: "Which sign does jQuery use as a shortcut?",
-    choices: ["The % sign" , "The $ sign", "The ? sign", "The # sign"],
-    correctAnswer: 1
-  }, {
-    question: "Which jQuery method is used to perform an asynchronous HTTP request?",
-    choices: ["jQuery.ajaxAsync()", "jQuery.ajaxSetup()", "jQuery.ajax()"],
-    correctAnswer: 2
-  }, {
-    question: "Which jQuery method returns the direct parent element of the selected element?",
-    choices: ["ancestors()", "ancestor()", "parents()", "parent()"],
-    correctAnswer: 3
-  }, {
-    question: "What was the name of the original Netscape Navigator?",
-    choices: ["Mozilla", "Safari", "Mosaic", "Web Explorer"],
-    correctAnswer: 0
-  }, {
-    question: "Which computer programming language was developed by Brendan Eich for Netscape in 1995?  Its intended purpose was to create or alter dynamic content in web browsers.",
-    choices: ["ActionScript", "MaxScript", "PostScript", "JavaScript"],
-    correctAnswer: 3
-  }, {
-    question: "Which is the correct CSS syntax?",
-    choices: ["{body:color=black;}", "body {color: black;}", "{body;color:black;}", "body:color=black;"],
-    correctAnswer: 1
-  }, {
-    question: "Is jQuery a library for client scripting or server scripting?",
-    choices: ["Client Scripting", "Server Scripting"],
-    correctAnswer: 0
-  }
+  question: "Who is JavaScript officially managed by?",
+  choices: ["Mozilla", "Microsoft", "Apple", "Sun Microsystems"],
+  correctAnswer: 0
+}, {
+  question: "What is Error 404?",
+  choices: ["Request Timeout", "Site not found in HTTP", "Bad File Request/Syntax Error", "Site Access Forbidden/Password Protected"],
+  correctAnswer: 1
+}, {
+  question: "Which if the following is correct?",
+  choices: ["jQuery is a JSON library", "jQuery is a JavaScript library"],
+  correctAnswer: 1
+}, {
+  question: "Which sign does jQuery use as a shortcut?",
+  choices: ["The % sign" , "The $ sign", "The ? sign", "The # sign"],
+  correctAnswer: 1
+}, {
+  question: "Which jQuery method is used to perform an asynchronous HTTP request?",
+  choices: ["jQuery.ajaxAsync()", "jQuery.ajaxSetup()", "jQuery.ajax()"],
+  correctAnswer: 2
+}, {
+  question: "Which jQuery method returns the direct parent element of the selected element?",
+  choices: ["ancestors()", "ancestor()", "parents()", "parent()"],
+  correctAnswer: 3
+}, {
+  question: "What was the name of the original Netscape Navigator?",
+  choices: ["Mozilla", "Safari", "Mosaic", "Web Explorer"],
+  correctAnswer: 0
+}, {
+  question: "Which computer programming language was developed by Brendan Eich for Netscape in 1995?  Its intended purpose was to create or alter dynamic content in web browsers.",
+  choices: ["ActionScript", "MaxScript", "PostScript", "JavaScript"],
+  correctAnswer: 3
+}, {
+  question: "Which is the correct CSS syntax?",
+  choices: ["{body:color=black;}", "body {color: black;}", "{body;color:black;}", "body:color=black;"],
+  correctAnswer: 1
+}, {
+  question: "Is jQuery a library for client scripting or server scripting?",
+  choices: ["Client Scripting", "Server Scripting"],
+  correctAnswer: 0
+}
 ];
 
 var questionCounter = 0;
